865d76cda479c5b1e5536b1245a869fbc703b0b9
Herman Cortez <herman.cortez@gmail.com>
2025-07-17 22:01:59 -0400

    feat: update second_test

--- tests/second_test.py
+++ tests/second_test.py
@@ -3,9 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options # Necesario para configurar Chrome
 
 def test_page_title():
-    # URL del Selenium Hub que se ejecuta como un servicio en GitHub Actions
-    # 'localhost' se refiere al servicio 'selenium' definido en el archivo YAML del workflow
-    selenium_hub_url = "http://localhost:4444/wd/hub"
+
 
     # Configurar opciones de Chrome para ejecución en un entorno de CI/CD (headless)
     chrome_options = Options()
@@ -13,21 +11,17 @@ def test_page_title():
     chrome_options.add_argument("--no-sandbox") # Necesario para entornos Linux/Docker (evita problemas de permisos)
     chrome_options.add_argument("--disable-dev-shm-usage") # Para evitar problemas de memoria compartida en Docker
 
-    # Inicializar el Remote WebDriver para conectarse al Selenium Hub
-    browser = webdriver.Remote(
-        command_executor=selenium_hub_url,
-        options=chrome_options # Pasar las opciones configuradas
-    )
 
     try:
+        driver = webdriver.Chrome(options=chrome_options)
         # Navegar a la página de GitHub
-        browser.get('https://github.com')
+        driver.get('https://github.com')
 
         # Para depuración: Imprimir el título de la página (aparecerá en los logs de GitHub Actions)
-        print(f"Page title is: {browser.title}")
+        print(f"Page title is: {driver.title}")
 
         # Encontrar el elemento por su ID
-        title_element = browser.find_element(By.ID, 'hero-section-brand-heading')
+        title_element = driver.find_element(By.ID, 'hero-section-brand-heading')
 
         # Realizar la aserción
         expected_text = 'Build and ship software on a single, collaborative platform'
@@ -36,4 +30,4 @@ def test_page_title():
     finally:
         # Siempre cierra el navegador al final para liberar recursos,
         # incluso si la prueba falla.
-        browser.quit()
+        driver.quit()
